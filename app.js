14c698a6e91570e6126f1942f40b51975aa6c5ab
kwonhygge <kitteniso1103@gmail.com>
2020-01-08 01:22:22 +0900

    Add database

--- app.js
+++ app.js
@@ -7,22 +7,37 @@ const _ = require('lodash');
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const posts=[];
 
 const app = express();
 
+mongoose.connect("mongodb://localhost:27017/blogDB",{useNewUrlParser: true, 
+useUnifiedTopology: true, 
+useCreateIndex: true, 
+useFindAndModify: false});
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  content: String
+});
+
+const Post = mongoose.model("Post",postSchema);
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/",function(req,res){
-  res.render("home",{homeStartingContent:homeStartingContent,posts:posts});
+
+  Post.find({},function(err,posts){
+    res.render("home",{homeStartingContent:homeStartingContent,posts:posts});
+  });
+  
   
 });
 
@@ -46,34 +61,36 @@ app.get("/compose",function(req,res){
 
 app.post("/compose",function(req,res){
 
-  const post = {
+  const post = new Post({
     title:req.body.postTitle,
     content:req.body.postBody
-  };
+  });
 
-  posts.push(post);
-  res.redirect("/");
+  post.save(function(err){
+    if(err){
+      console.log(err);
+      
+    }else{
+      res.redirect("/");
+    }
+  })
+  
 
 });
 
 //lodash 는 유용한 자바스크립트 라이브러리
-app.get("/post/:postName",function(req,res){
-  const requestedTitle = _.lowerCase(req.params.postName);
+app.get("/post/:postId", function(req, res){
+
+  const requestedPostId = req.params.postId;
   
-  posts.forEach(function(post){
-    const savedTitle=_.lowerCase(post.title);
-    if (savedTitle === requestedTitle ){
-      res.render("post",{
+    Post.findOne({_id: requestedPostId}, function(err, post){
+      res.render("post", {
         title: post.title,
         content: post.content
       });
-    }
+    });
+  
   });
-});
-
-app.post("/post/:postName",function(req,res){
-  console.log("hello");
-});
 
 
 app.listen(3000, function() {
